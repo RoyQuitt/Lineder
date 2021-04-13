fe124db31954cb6ef6c5e8ffb62d28f004b6f4e5
RoyQuitt <roy.quitt@gmail.com>
2021-04-13 20:30:34 +0300

    added dynamic port to test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -14,4 +16,6 @@ def test():
 
 
 if __name__ == "__main__":
+    port = os.environ.get('PORT')
+    app.run(ssl_context="adhoc", host="0.0.0.0", port=port)
     app.run()
