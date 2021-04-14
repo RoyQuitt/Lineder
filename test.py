1b29e0e5a71cd3faba8054244177a97ea3e25baa
RoyQuitt <58001703+RoyQuitt@users.noreply.github.com>
2021-04-14 18:06:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -20,4 +20,4 @@ def test():
 
 if __name__ == "__main__":
     port = os.environ.get('PORT')
-    app.run(ssl_context="adhoc", host="0.0.0.0", port=port)
+    #app.run(ssl_context="adhoc", host="0.0.0.0", port=port)
